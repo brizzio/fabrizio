ff383fc30608049bbc54776e9a857ea38231c3b0
brizzio <fabrizio.salvade@gmail.com>
2019-01-03 00:54:49 -0200

    materialize

---
+++ main.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = {};
+    var instances = M.Sidenav.init(elems, options);
+    console.log('logou')
+  });
